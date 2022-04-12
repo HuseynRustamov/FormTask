638e297c2d88f08ed3cb1b6ba9d5d0757aad55a1
Rust_ls55 <Rust_ls55@STHQ0118-12>
2022-04-12 09:32:21 +0400

    Add project files.

---
+++ FormTask/FakeRepo/UserRepo.cs
@@ -0,0 +1,32 @@
+﻿using FormTask.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormTask.FakeRepo
+{
+    public class UserRepo
+    {
+        public static List<User> Users { get; set; } = new List<User>
+        {
+                new User
+                {
+                    Id=1,
+                    Name="Elton",
+                    Surname="Johnlu",
+                    ImagePath="image1.png",
+                    Age=55
+                },
+                new User
+                {
+                    Id=2,
+                    Name="John ",
+                    Surname="Johnlu",
+                    ImagePath="image2.jpeg",
+                    Age=88
+                }
+             
+        };
+    }
+}
